2a1c087fc1d30d02d54a046c8463d97c743802ce
Bartłomiej Bielicki <bartlomiej.bielicki@certusoft.pl>
2018-09-08 11:32:07 +0200

    [#2.2]

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -10,22 +10,26 @@
       <li v-for="p in products" :key="p.id">{{ p.name }}</li>      
       <!-- <li v-fonpmr="p in products">{{ p.name }}
       <button v-on:click="removeSelected(p.id)">Remove</button> -->
-    </li>
+    <!-- </li> -->
     </ul>
     <!-- 5. v-if can be helpful with conditional statements -->
     <p v-if="!products.length">No products!</p>
     <!-- 6. v-on adds an handler and :click is the name of the event, then goes the function to invoke -->
     <button v-on:click="removeLast()">Remove last item</button>
-    <!-- <button v-on:click="addNew(product)">Add new item</button>
-    <input v-model="product"  placeholder="add multiple lines"> -->
+    <button v-on:click="addNew(product)">Add new item</button>
+    <input v-model="product"  placeholder="add multiple lines">
   </div>
 </template>
 
 <!--21- 2. A <script> part -->
 <script>
 // 4. Now App is not mounted itself, we're just creating a component (more on that later - hold your horses!)
+import uuid from 'uuid/v4';
+
 export default {
   name: 'app',
+  product:'',
+
   //11/ 5. Data can no longer be just an object to prevent accidental shared state
   data() {
     return {
@@ -41,6 +45,12 @@ export default {
   methods: {
     removeLast() {
       this.products.pop();
+    },
+    addNew(product) {
+      this.products.push({
+          id:uuid(),
+          name:product
+      });
     }
   }
 }
